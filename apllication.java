7228eee62e1d7d2f4f7f51042d4b0060575ec654
belief2507 <belief2507@gmail.com>
2024-05-17 22:28:48 +0530

    feature202 changes done

--- apllication.java
+++ apllication.java
@@ -40,4 +40,21 @@ class geeksforgeeks {
 			"Even sum of fibonacci series till number " + N
 			+ " is: " + +Fib_Even_Sum(N));
 	}
+#feature202 changes by karthik on oct.2023
+class Main {
+  public static void main(String[] args) {
+
+    int n = 10, firstTerm = 0, secondTerm = 1;
+    System.out.println("Fibonacci Series till " + n + " terms:");
+
+    for (int i = 1; i <= n; ++i) {
+      System.out.print(firstTerm + ", ");
+
+      // compute the next term
+      int nextTerm = firstTerm + secondTerm;
+      firstTerm = secondTerm;
+      secondTerm = nextTerm;
+    }
+  }
+}
 }
